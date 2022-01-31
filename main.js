b1d7f2ee2990640222cd0d1f32710f77be2bd3b1
rakibul-cse-diu <rakibul2180@gmail.com>
2022-01-31 10:33:08 +0600

    add dynamicness for flipp colors

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const Color = ["#FF5733", "#FFBD33", "#DBFF33", "#75FF33", "#33FF57", "#33FFBD", "#00ADFF", "#0052FF", "#4700FF", "#8900FF", "#FB00FF", "#FF006E", "#FF0000"];
 
+const container = document.querySelector(".content-wrapper");
+const colorName = document.querySelector(".color-code");
+const Button = document.querySelector(".btn");
+
+function handleColorFlipp() {
+    const RandomNumber = Math.floor(Math.random() * Color.length);
+
+    container.style.backgroundColor = Color[RandomNumber];
+    colorName.innerHTML = Color[RandomNumber];
+}
